e9f34b8a138b207bd73b4c92cee7a84b93a02721
kepop1 <kepop123@gmail.com>
2020-09-28 09:24:51 +0100

    Fixed it

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,14 @@
-// $(document).ready(function () {
-function setModalSource(event) {
-  var triggerElement = event.currentTarget;
-  var videoId = +triggerElement.dataset.source;
-  var iframe = $("#iframeVideo")[0];
-  var player = new Vimeo.Player(iframe);
+$(document).ready(function () {
+  function setModalSource(event) {
+    var triggerElement = event.currentTarget;
+    var videoId = triggerElement.dataset.source;
+    var iframe = $("#iframeVideo")[0];
+    var player = new Vimeo.Player(iframe);
 
-  if (videoId) {
-    player
-      .loadVideo(videoId)
-      .then(function () {
-        console.log("success");
-      })
-      .catch(function () {
-        console.log("failed");
-      });
+    if (videoId) {
+      player.loadVideo(videoId);
+    }
   }
-}
 
-$(".launchModal").click(setModalSource);
-// });
+  $(".launchModal").click(setModalSource);
+});
