2afddd8adc798f208e9c16eb19c6674fc6120948
kepop1 <kepop123@gmail.com>
2020-09-28 09:15:24 +0100

    Final test before breaking

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,19 @@
 $(document).ready(function () {
   function setModalSource(event) {
     var triggerElement = event.currentTarget;
+    var videoId = triggerElement.dataset.source;
     var iframe = $("#iframeVideo")[0];
     var player = new Vimeo.Player(iframe);
 
-    if (triggerElement.dataset.source) {
-      player.loadVideo(triggerElement.dataset.source);
+    if (videoId) {
+      player
+        .loadVideo(videoId)
+        .then(function () {
+          console.log("success");
+        })
+        .catch(function () {
+          console.log("failed");
+        });
     }
   }
 
