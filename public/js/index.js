35de4b1d0a13895f0d19b11ccdbf2fb6aacb9bc5
kepop1 <kepop123@gmail.com>
2020-09-28 08:01:08 +0100

    Added modal functionality

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,14 @@
-//
+$(document).ready(function () {
+  function setModalSource(event) {
+    var triggerElement = event.currentTarget;
+
+    if (triggerElement.dataset.source) {
+      $("#iframeVideo")[0].src =
+        "https://player.vimeo.com/video/" +
+        triggerElement.dataset.source +
+        "?autoplay=1&loop=1&title=0&byline=0&portrait=0";
+    }
+  }
+
+  $(".launchModal").click(setModalSource);
+});
