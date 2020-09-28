687432f8d91dd1c05b1f890b6be2f8bb73d9dc8f
kepop1 <kepop123@gmail.com>
2020-09-28 08:39:40 +0100

    Test out a fix

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,12 @@
-$(document).ready(function () {
-  function setModalSource(event) {
-    var triggerElement = event.currentTarget;
+function setModalSource(event) {
+  var triggerElement = event.currentTarget;
 
-    if (triggerElement.dataset.source) {
-      $("#iframeVideo")[0].src =
-        "https://player.vimeo.com/video/" +
-        triggerElement.dataset.source +
-        "?autoplay=1&loop=1&title=0&byline=0&portrait=0";
-    }
+  if (triggerElement.dataset.source) {
+    $("#iframeVideo")[0].src =
+      "https://player.vimeo.com/video/" +
+      triggerElement.dataset.source +
+      "?autoplay=1&loop=1&title=0&byline=0&portrait=0";
   }
+}
 
-  $(".launchModal").click(setModalSource);
-});
+$(".launchModal").click(setModalSource);
