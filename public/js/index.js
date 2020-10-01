87b31b1adfcad4685308e30e86046e4e28355e6f
kepop1 <kepop123@gmail.com>
2020-10-01 08:58:21 +0100

    Stop the video modal playing when the modal is hidden or the browser is refreshed

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,28 @@
 $(document).ready(function () {
+  var iframe = $("#iframeVideo")[0];
+  var player = new Vimeo.Player(iframe);
+
+  // Stop it playing if the browser is refreshed
+  stopPlayer();
+
   function setModalSource(event) {
     var triggerElement = event.currentTarget;
     var videoId = triggerElement.dataset.source;
-    var iframe = $("#iframeVideo")[0];
-    var player = new Vimeo.Player(iframe);
 
+    // Set the new video id if required.
     if (videoId) {
       player.loadVideo(videoId);
+      player.setAutopause(true);
     }
   }
 
+  function stopPlayer() {
+    // When hiding the modal stop it and reset
+    player.pause();
+    player.unload();
+  }
+
   $(".launchModal").click(setModalSource);
+
+  $("#videoModal").on("hidden.bs.modal", stopPlayer);
 });
